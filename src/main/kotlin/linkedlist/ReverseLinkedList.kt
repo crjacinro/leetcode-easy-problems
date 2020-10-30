dd477e062842c39c2355b31bd3f3698afa0f2791
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2020-10-30 17:17:36 +0800

    Add recursive solution

--- src/main/kotlin/linkedlist/ReverseLinkedList.kt
+++ src/main/kotlin/linkedlist/ReverseLinkedList.kt
@@ -14,3 +14,15 @@ fun reverseList(head: ListNode?): ListNode? {
 
     return previousNode
 }
+
+fun reverseListRecursive(head: ListNode?): ListNode? {
+    return reverse(null, head)
+}
+
+fun reverse(previousNode: ListNode?, currentNode: ListNode?): ListNode? {
+    val nextNode: ListNode? = currentNode?.next
+    currentNode?.next = previousNode
+
+    if (nextNode == null) return currentNode
+    return reverse(currentNode, nextNode)
+}
