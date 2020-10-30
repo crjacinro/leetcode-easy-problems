debab7660d6a03d1b0c87fa9298222b358f00b40
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2020-10-30 17:28:15 +0800

    Add comments on the recursive solution

--- src/main/kotlin/linkedlist/ReverseLinkedList.kt
+++ src/main/kotlin/linkedlist/ReverseLinkedList.kt
@@ -19,10 +19,18 @@ fun reverseListRecursive(head: ListNode?): ListNode? {
     return reverse(null, head)
 }
 
+// Recursive function receives previous and current
+// so that it can set the next of current to previous
 fun reverse(previousNode: ListNode?, currentNode: ListNode?): ListNode? {
+    // get copy of the next node first
     val nextNode: ListNode? = currentNode?.next
+    // set the next of the current node to the previous
     currentNode?.next = previousNode
 
+    // if next is null, we are in the end.
+    // just return the current node since this is the new head
     if (nextNode == null) return currentNode
+
+    // recursively do this with the current node and the next node
     return reverse(currentNode, nextNode)
 }
