07d76e3b9b898b2267b878fa6d61fc95c3ccda60
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2020-10-31 18:12:08 +0800

    Add merge sorted list solution with comments

--- src/main/kotlin/linkedlist/MergeSortedList.kt
+++ src/main/kotlin/linkedlist/MergeSortedList.kt
@@ -2,8 +2,6 @@ package linkedlist
 
 
 fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
-    if (l1 == null && l2 == null) return null
-
     var currentLeft = l1
     var currentRight = l2
     var newSorted: ListNode? = null
@@ -11,163 +9,54 @@ fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
     var shouldContinue = true
 
     while (shouldContinue) {
-        if (currentLeft != null && currentRight != null) {
-            if (currentLeft.`val` < currentRight.`val`) {
-                val newNode = ListNode(currentLeft.`val`)
-                if (newSorted == null) {
-                    newSorted = newNode
-                    head = newSorted
-                } else {
-                    newSorted.next = newNode
-                    newSorted = newSorted.next
-                }
+        val nodeToAdd: ListNode
+        var nodeCountToAdd = 1
 
-                currentLeft = currentLeft.next
-            } else if (currentLeft.`val` > currentRight.`val`) {
-                val newNode = ListNode(currentRight.`val`)
-                if (newSorted == null) {
-                    newSorted = newNode
-                    head = newSorted
-                } else {
-                    newSorted.next = newNode
-                    newSorted = newSorted.next
-                }
-
-                currentRight = currentRight.next
-            } else {
-                val newNode = ListNode(currentRight.`val`)
-                if (newSorted == null) {
-                    newSorted = newNode
-                    head = newSorted
-                } else {
-                    newSorted.next = newNode
-                    newSorted = newSorted.next
-                }
-
-                val newNode2 = ListNode(currentLeft.`val`)
-                newSorted?.next = newNode2
-                newSorted = newSorted?.next
-
-                currentRight = currentRight.next
-                currentLeft = currentLeft.next
-            }
-        } else if (currentLeft == null && currentRight != null) {
-            val newNode = ListNode(currentRight.`val`)
-            if (newSorted == null) {
-                newSorted = newNode
-                head = newSorted
-            } else {
-                newSorted.next = newNode
-                newSorted = newSorted.next
-            }
+        if (currentLeft == null && currentRight == null) {
+            return null
+        } else if ((currentLeft == null && currentRight != null)) {
+            // Only right side of the element is added. No more elements on the left
+            nodeToAdd = currentRight
+            currentRight = currentRight.next
+        } else if ((currentLeft != null && currentRight == null)) {
+            // Only left side of the element is added.  No more elements on the right
+            nodeToAdd = currentLeft
+            currentLeft = currentLeft.next
+        } else if (currentLeft!!.`val` > currentRight!!.`val`) {
+            // Only right side of the element is added since it is lesser
+            nodeToAdd = currentRight
+            currentRight = currentRight.next
+        } else if (currentLeft.`val` < currentRight.`val`) {
+            // Only left side of the element is added since it is lesser
+            nodeToAdd = currentLeft
+            currentLeft = currentLeft.next
+        } else {
+            // Both elements are equal. Add this element twice
+            nodeToAdd = currentLeft
+            nodeCountToAdd = 2
 
             currentRight = currentRight.next
-        } else if (currentLeft != null && currentRight == null) {
-            val newNode = ListNode(currentLeft.`val`)
-            if (newSorted == null) {
-                newSorted = newNode
-                head = newSorted
-            } else {
-                newSorted.next = newNode
-                newSorted = newSorted.next
-            }
-
             currentLeft = currentLeft.next
         }
 
+        for (i in 0 until nodeCountToAdd) {
+            // Create a copy of the node to be added to the sorted list
+            val newNode: ListNode? = ListNode(nodeToAdd.`val`)
+
+            if (newSorted == null) {
+                // if this is the first element, make it the head
+                newSorted = newNode
+                head = newSorted
+            } else {
+                // Append the node to the sorted list and move to the tip
+                newSorted.next = newNode
+                newSorted = newSorted.next
+            }
+        }
+
+        // We will continue if there are still elements either on left or right list
         shouldContinue = currentLeft != null || currentRight != null
     }
 
     return head
 }
-
-
-/*
-    while (currentLeft != null) {
-        nextLeft = currentLeft.next
-
-        while (currentRight != null) {
-            nextRight = currentRight.next
-
-            if (currentLeft.`val` > currentRight.`val`) {
-                if (current == null) {
-                    head = currentRight
-                }
-
-                currentRight.next = currentLeft
-                current = currentLeft
-
-            } else if (currentLeft.`val` < currentRight.`val`) {
-                if (current == null) {
-                    head = currentLeft
-                }
-
-                currentLeft.next = currentRight
-                current = currentRight
-            }
-
-
-            currentRight = nextRight
-        }
-
-        currentLeft = nextLeft
-    }
- */
-
-
-/*
-
-  if(currentLeft == null){
-            if (newSorted == null) {
-                newSorted = ListNode(currentRight!!.`val`)
-                head = newSorted
-            } else {
-                newSorted.next = ListNode(currentRight!!.`val`)
-                newSorted = newSorted.next
-            }
-            currentRight = currentRight.next
-            continue
-        }
-        if(currentRight == null){
-            if (newSorted == null) {
-                newSorted = ListNode(currentLeft.`val`)
-                head = newSorted
-            } else {
-                newSorted.next = ListNode(currentLeft.`val`)
-                newSorted = newSorted.next
-            }
-            currentLeft = currentLeft.next
-            continue
-        }
-
-
-        if (currentLeft.`val` < currentRight.`val`) {
-            if (newSorted == null) {
-                newSorted = ListNode(currentLeft.`val`)
-                head = newSorted
-            } else {
-                newSorted.next = ListNode(currentLeft.`val`)
-                newSorted = newSorted.next
-            }
-            currentLeft = currentLeft.next
-        } else if (currentLeft.`val` > currentRight.`val`) {
-            if (newSorted == null) {
-                newSorted = ListNode(currentRight.`val`)
-                head = newSorted
-            } else {
-                newSorted.next = ListNode(currentRight.`val`)
-                newSorted = newSorted.next
-            }
-            currentRight = currentRight.next
-        } else{
-            if (newSorted == null) {
-                newSorted = ListNode(currentRight.`val`)
-                head = newSorted
-            } else {
-                newSorted.next = ListNode(currentRight.`val`)
-                newSorted = newSorted.next
-            }
-            currentRight = currentRight.next
-        }
-
- */
