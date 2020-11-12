08ccc7d62612543852e4b57bdd6f3339cf190d22
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2020-11-12 22:45:41 +0800

    Add memoization

--- src/main/kotlin/dynamicprogramming/ClimbStairs.kt
+++ src/main/kotlin/dynamicprogramming/ClimbStairs.kt
@@ -1,15 +1,19 @@
 package dynamicprogramming
 
 fun climbStairs(n: Int): Int {
-    return dp(0, n)
+    return dp(0, n, hashMapOf())
 }
 
-private fun dp(step: Int, n: Int): Int {
+private fun dp(step: Int, n: Int, mem: HashMap<Int, Int>): Int {
+    if (mem.containsKey(step)) return mem[step]!!
     if (n - step == 1) return 1
     if (n - step == 2) return 2
 
-    val oneStep = dp(step + 1, n)
-    val twoStep = dp(step + 2, n)
+    val oneStep = dp(step + 1, n, mem)
+    val twoStep = dp(step + 2, n, mem)
+    val result = oneStep + twoStep
 
-    return oneStep + twoStep
+    mem[step] = result
+
+    return result
 }
