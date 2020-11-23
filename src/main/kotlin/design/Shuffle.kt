93bcbf008c97e2b731ded4ba2395c9f0a63b6878
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2020-11-23 22:09:20 +0800

    Add easy shuffle solution

--- src/main/kotlin/design/Shuffle.kt
+++ src/main/kotlin/design/Shuffle.kt
@@ -1,47 +1,38 @@
 package design
 
-class Shuffle(val nums: IntArray) {
-    private val original = nums.copyOf()
-    private val newIndexMap = hashMapOf<Int, Int>()
+class Shuffle(nums: IntArray) {
+    private val original = nums.asList()
 
     /** Resets the array to its original configuration and return it. */
     fun reset(): IntArray {
-        original.copyInto(nums)
-        return original
+        return original.toIntArray()
     }
 
     /** Returns a random shuffling of the array. */
     fun shuffle(): IntArray {
-        newIndexMap.clear()
-        updateShuffleMap()
-
-        for (i in nums.indices) {
-            nums[i] = newIndexMap[i]!!
-        }
-
-        return nums
+        return original.shuffled().toIntArray()
     }
 
-    /** Returns a random shuffling of the array. */
-    fun shuffleSlow(): IntArray {
-        newIndexMap.clear()
-        updateShuffleMap()
-
-        for (i in nums.indices) {
-            nums[i] = newIndexMap[i]!!
-        }
-
-        return nums
-    }
-
-    private fun updateShuffleMap() {
-        for (i in nums.indices) {
-            var random = 0
-            do {
-                random = (nums.indices).random()
-            } while (newIndexMap.containsKey(random))
-
-            newIndexMap[random] = nums[i]
-        }
-    }
+//    /** Returns a random shuffling of the array. */
+//    fun shuffleSlow(): IntArray {
+//        newIndexMap.clear()
+//        updateShuffleMap()
+//
+//        for (i in nums.indices) {
+//            nums[i] = newIndexMap[i]!!
+//        }
+//
+//        return nums
+//    }
+//
+//    private fun updateShuffleMap() {
+//        for (i in nums.indices) {
+//            var random = 0
+//            do {
+//                random = (nums.indices).random()
+//            } while (newIndexMap.containsKey(random))
+//
+//            newIndexMap[random] = nums[i]
+//        }
+//    }
 }
