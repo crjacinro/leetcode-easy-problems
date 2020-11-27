a6f35b7ccce386459df7eabefe3d518b6aa7ab50
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2020-11-27 23:10:00 +0800

    Add count prime (n^2 solution - time limit exceeded)

---
+++ src/main/kotlin/math/CountPrime.kt
@@ -0,0 +1,22 @@
+package math
+
+fun countPrimes(n: Int): Int {
+    if (n == 0 || n == 1) return 0
+
+    var count = 0
+    for (i in 1 until n) {
+        if (isPrime(i))
+            count++
+
+    }
+    return count
+}
+
+private fun isPrime(n: Int): Boolean {
+    if (n == 0 || n == 1) return false
+
+    for (i in 2 until n) {
+        if (n % i == 0) return false
+    }
+    return true
+}
---
+++ src/test/kotlin/math/CountPrimesTest.kt
@@ -0,0 +1,16 @@
+package math
+
+import org.junit.Assert
+import org.junit.Test
+
+class CountPrimesTest {
+    @Test
+    fun should_return_primes() {
+        val input = 499979
+        val expected = 4
+
+        val result = countPrimes(input)
+
+        Assert.assertEquals(expected, result)
+    }
+}
